44798a2866cbc37544ccd43e41e1c65529621112
klotzgal <ggg44466635@gmail.com>
2023-07-23 14:17:13 +0300

    Part 4

--- src/Part03/main.c
+++ src/Part03/main.c
@@ -1,15 +1,12 @@
-// #include <fcgiapp.h>
 #include <fcgi_stdio.h>
-// #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
-    while(FCGI_Accept() >= 0)
-      printf("Content-type: text/html\r\n"
-               "\r\n"
-
-               "<title>Hello World!</title>"
-               "<h1>Hello World!</h1>");
-    return 0;
+int main(void) {
+  while (FCGI_Accept() >= 0)
+    printf(
+        "Content-type: text/html\r\n"
+        "\r\n"
+        "<title>Hello World!</title>"
+        "<h1>Hello World!</h1>");
+  return 0;
 }
