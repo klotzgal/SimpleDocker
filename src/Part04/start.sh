2f7e82e45fa409efd9e34f1eabacd2e2bf7cdc4e
klotzgal <ggg44466635@gmail.com>
2023-07-24 21:15:48 +0300

    Колдую с докерфайлом

--- src/Part04/start.sh
+++ src/Part04/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcc main.c -lfcgi -o /src/main
-# ln /etc/nginx/nginx.conf Part03/nginx.conf
+# # ln /etc/nginx/nginx.conf Part03/nginx.conf
 spawn-fcgi -p 8080 -f /src/main
 nginx -g "daemon off;"
